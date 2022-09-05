d9aa598db07529603ec851cdcefa8ecad4fc84ca
fatih <alfatihrizqialfian@gmail.com>
2022-09-05 22:35:04 +0700

    rapihin and adding style

--- src/components/UsersTodo.js
+++ src/components/UsersTodo.js
@@ -5,59 +5,73 @@ import TodoCard from "./style/TodoCard";
 const UsersTodo = (props) => {
   const [popUp, setPopUp] = useState(false);
   const [editKey, setEditKey] = useState("");
+  const [checkbox, setCheckbox] = useState(false);
 
-  const exitPopup = ()=>{
+  const exitPopup = () => {
     setPopUp(!popUp);
     setEditKey("");
-  }
+  };
 
-  const deleteHandler = ()=>{
-    const newList = props.usersTodo.filter(x=>x.key !== editKey);
-    props.onChange(newList) 
-  }
+  const deleteHandler = () => {
+    const newList = props.usersTodo.filter((x) => x.key !== editKey);
+    props.onChange(newList);
+  };
 
-  const updateListInput =(newList)=>{
-    const index = props.usersTodo.findIndex(x => x.key === editKey);
-    console.log(index)
-    console.log(props.usersTodo[index])
-    props.usersTodo[index] = {todolist: newList, key: editKey}
-    console.log(props.usersTodo[index])
-    props.onChange(props.usersTodo) 
-  }
+  const updateListInput = (newList) => {
+    const index = props.usersTodo.findIndex((x) => x.key === editKey);
+    console.log(index);
+    console.log(props.usersTodo[index]);
+    props.usersTodo[index] = { todolist: newList, key: editKey };
+    console.log(props.usersTodo[index]);
+    props.onChange(props.usersTodo);
+  };
 
   return (
     <>
-      {popUp && <Popup onExit={exitPopup} onDeletePopup={deleteHandler} onEdit={updateListInput}/>}
+      {popUp && (
+        <Popup
+          onExit={exitPopup}
+          onDeletePopup={deleteHandler}
+          onEdit={updateListInput}
+        />
+      )}
       <div className="flex flex-row justify-evenly overflow-visible todolist overflow-y-scroll">
         <div className="flex flex-col">
-          <h1 className="text-josefin text-2xl font-semibold text-center">
-            ACTIVITIES
-          </h1>
-
           {props.usersTodo.map((lists) => (
-            <div className="flex flex-row">
-              <TodoCard key={lists.key} onClick={exitPopup}>
-                <ul className="text-poppins">
-                  <li>
-                    {lists.todolist}
-                  </li>
-                </ul>
-                <div className="">
-                  <ion-icon
-                    name="ellipsis-vertical-outline"
-                    onClick={() => {
-                      setPopUp(!popUp);
-                      setEditKey(lists.key);
-                    }}
-                  ></ion-icon>
-                </div>
-              </TodoCard>
-              <input
-                type="checkbox"
-                value="done"
-                className="ml-12 mt-4 w-8 h-8"
-              ></input>
-            </div>
+            <>
+              <div className="flex flex-row text-josefin text-2xl font-semibold text-center">
+                <h1 className="relative left-[40%]">ACTIVITIES</h1>
+                <h1 className="relative left-[85%]">DONE</h1>
+              </div>
+              <div className="flex flex-row">
+                <TodoCard
+                  key={lists.key}
+                  onClick={exitPopup}
+                  checkboxStatus={checkbox}
+                >
+                  <ul className="text-poppins">
+                    <li>{lists.todolist}</li>
+                  </ul>
+                  <div className="">
+                    <ion-icon
+                      name="ellipsis-vertical-outline"
+                      onClick={() => {
+                        setPopUp(!popUp);
+                        setEditKey(lists.key);
+                      }}
+                    ></ion-icon>
+                  </div>
+                </TodoCard>
+                <input
+                  type="checkbox"
+                  value="done"
+                  className="ml-12 mt-4 w-8 h-8"
+                  onClick={() => {
+                    setCheckbox(!checkbox);
+                  }}
+                ></input>
+              </div>
+            </>
           ))}
         </div>
       </div>
--- src/components/style/TodoCard.js
+++ src/components/style/TodoCard.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const TodoCard = (props) => {
-
   return (
-    <div className="bg-[#F4EDE6] mt-3 flex flex-row justify-between w-[1000px] py-2 px-4 rounded">
-      {props.children}
-    </div>
+    <>
+      {props.checkboxStatus ? (
+        <div className="bg-[#2F6568] mt-3 flex flex-row justify-between w-[1000px] py-2 px-4 rounded text-white">
+          {props.children}
+        </div>
+      ) : (
+        <div className="bg-[#F4EDE6] mt-3 flex flex-row justify-between w-[1000px] py-2 px-4 rounded">
+          {props.children}
+        </div>
+      )}
+    </>
   );
 };
 
